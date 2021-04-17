9272cf5ce513fc3e51af79ca1415ff8dd5a7b537
David Bendiksen <davidbendiksen@hotmail.com>
2021-04-17 14:36:26 +0200

    Removed superflous DC handling

--- socketIO.js
+++ socketIO.js
@@ -6,11 +6,6 @@ let p;  // Port
 let io;
 let httpServer;
 let clients = [];
-const explicitDCs = [
-  'server namespace disconnect',
-  'client namespace disconnect',
-  'server shutting down'
-];
 
 const startServer = (hostname = '127.0.0.1', port = 3000) => {
   hn = hostname;
@@ -19,10 +14,6 @@ const startServer = (hostname = '127.0.0.1', port = 3000) => {
   io = new Server(httpServer);
 
   io.on('connection', (socket) => {
-    if (clients.indexOf(socket) > -1) {
-      console.log(`Client "${socket.id}" reconnected`);
-      return;
-    }
     console.log(`New connection: ${socket.id}`);
     clients.push(socket);
     socket.on('rubbish', () => console.log('ribbush'));
@@ -35,7 +26,6 @@ const startServer = (hostname = '127.0.0.1', port = 3000) => {
 }
 
 const clientDC = (client, reason) => {
-  if (explicitDCs.indexOf(reason) == -1) return;
   const index = clients.indexOf(client);
   clients.splice(index, 1);
   console.log(`Client "${client.id}" disconnected: ${reason}`);
