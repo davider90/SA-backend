010cc6c6d84e16afc42878185780784eafc48d84
David Bendiksen <davidbendiksen@hotmail.com>
2021-04-30 17:18:54 +0200

    Address update

--- socketTest.js
+++ socketTest.js
@@ -1,12 +1,13 @@
 import { io } from "socket.io-client";
 
-const socket = io('http://127.0.0.1:3000/', {
+const socket = io('http://35.228.7.69:3000/', {
   auth: {
     name: 'David',
     password: 'test',
     new: 'false'
   }
 });
+socket.on('disconnect', (reason) => console.log(reason));
 socket.on('connect_error', (err) => console.log(err))
 socket.on('connect', () => console.log(socket.id));
 setTimeout(() => socket.emit('getTopTen', (response) => {
