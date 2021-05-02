cbcad24f2220447271f641311da50341e9f3d5fc
David Bendiksen <davidbendiksen@hotmail.com>
2021-05-02 16:53:59 +0200

    Added missing semicolon

--- socketTest.js
+++ socketTest.js
@@ -17,7 +17,7 @@ const socket = io('http://35.228.7.69:3000/', {
 //   }
 // });
 socket.on('disconnect', (reason) => console.log(reason));
-socket.on('connect_error', (err) => console.log(err))
+socket.on('connect_error', (err) => console.log(err));
 socket.on('connect', () => console.log(socket.id));
 setTimeout(() => socket.emit('getTopTen', (response) => {
   console.log(response);
