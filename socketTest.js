9e69d76c9807cb371843691f9927bb3f8ad354d9
David Bendiksen <davidbendiksen@hotmail.com>
2021-05-02 16:42:44 +0200

    Added more testing

--- socketTest.js
+++ socketTest.js
@@ -7,10 +7,18 @@ const socket = io('http://35.228.7.69:3000/', {
     new: 'false'
   }
 });
+// const socket = io('http://127.0.0.1:3000/', {
+//   auth: {
+//     name: 'David',
+//     password: 'test',
+//     new: 'false'
+//   }
+// });
 socket.on('disconnect', (reason) => console.log(reason));
 socket.on('connect_error', (err) => console.log(err))
 socket.on('connect', () => console.log(socket.id));
 setTimeout(() => socket.emit('getTopTen', (response) => {
   console.log(response);
-}), 4000);
-setTimeout(() => socket.disconnect(), 8000);
+}), 2000);
+// socket.on('gameRequest', (...args) => console.log(args));
+setTimeout(() => socket.disconnect(), 6000);
